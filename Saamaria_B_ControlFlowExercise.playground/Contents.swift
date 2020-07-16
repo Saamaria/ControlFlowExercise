f89fa3ad76b195f404b74c6f542976c647a4bab6
Saamaria <67989662+Saamaria@users.noreply.github.com>
2020-07-15 21:36:31 -0500

    Add files via upload

---
+++ Saamaria_B_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,12 @@
+for index in 1...5{
+    print("\(index) times 5 is \(index * 5)")
+}
+
+
+var temperatureInfahrenheit = 63
+
+if temperatureInfahrenheit <= 32 {
+    print("It's very cold. Consider wearing a bigger jacket.")
+} else {
+print("It's not that cold. wear a t-shirt.")
+}
